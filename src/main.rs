f03d9c77bc22e1593c19878b7ff6a3364a2514ef
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-07 11:22:14 +0100

    rassemblement des arguments dans une structure + separation de leur extraction dans une fonction separee

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,25 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let recherche = &args[1];
-    let nom_fichier = &args[2];
+    let config = interpreter_config(&args);
 
-    println!("On recherche : {}", recherche);
-    println!("Dans le fichier : {}", nom_fichier);
+    println!("On recherche : {}", config.recherche);
+    println!("Dans le fichier : {}", config.nom_fichier);
 
-    let contenu = fs::read_to_string(nom_fichier)
+    let contenu = fs::read_to_string(config.nom_fichier)
         .expect("Quelquechose ne s'est pas passé comme prévu lors
             de la lecture du fchier");
     println!("Dans le texte: \n{}", contenu);
+}
+
+struct Config {
+    recherche:String,
+    nom_fichier: String,
+}
+
+fn interpreter_config(args: &[String]) -> Config {
+    let recherche = args[1].clone();
+    let nom_fichier = args[2].clone();
+
+    Config { recherche, nom_fichier }
 }
