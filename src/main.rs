1e98ab8a9b08f12e198a5c5e6ebf092d9a32942e
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-07 11:34:44 +0100

    meilleure gestion d'erreurs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        process::exit(1);
+    });
 
     println!("On recherche : {}", config.recherche);
     println!("Dans le fichier : {}", config.nom_fichier);
