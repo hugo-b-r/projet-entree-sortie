be9f9fe309c776c4931150299efce05ff6b7ea4a
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-07 11:08:54 +0100

    debut...

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let recherche = &args[1];
+    let nom_fichier = &args[2];
+
+    println!("On recherche : {}", recherche);
+    println!("Dans le fichier : {}", nom_fichier);
 }
