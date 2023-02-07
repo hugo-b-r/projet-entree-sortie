7672a235fa5227f10a7cea9ae77f3dc34cc8ba8e
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-07 11:12:41 +0100

    on ouvre le fichier pour ensuite le lire

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +10,9 @@ fn main() {
 
     println!("On recherche : {}", recherche);
     println!("Dans le fichier : {}", nom_fichier);
+
+    let contenu = fs::read_to_string(nom_fichier)
+        .expect("Quelquechose ne s'est pas passé comme prévu lors
+            de la lecture du fchier");
+    println!("Dans le texte: \n{}", contenu);
 }
