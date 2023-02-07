eeca87afc183af6f17986330201d8a2bf2426926
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-07 11:46:43 +0100

    separation dans un fichier de bibliotheque

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,7 +15,7 @@ fn main() {
     println!("On recherche : {}", config.recherche);
     println!("Dans le fichier : {}", config.nom_fichier);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Erreur applicative : {}", e);
 
         process::exit(1);
@@ -24,28 +23,3 @@ fn main() {
 }
 
 
-fn run(config:Config) -> Result <(), Box<dyn Error>> {
-    let contenu = fs::read_to_string(config.nom_fichier)?;
-
-            
-    println!("Dans le texte: \n{}", contenu);
-
-    Ok(())
-}
-
-struct Config {
-    recherche:String,
-    nom_fichier: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("il n'y a pas assez d'arguments");
-        }
-        let recherche = args[1].clone();
-        let nom_fichier = args[2].clone();
-
-        Ok(Config { recherche, nom_fichier })
-    }
-}
